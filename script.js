f8397a278b502aae143494b3d512ecb7fc488c24
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-05-29 14:41:51 +0100

    some starter javas - for hiding/showing pages/questions

---
+++ script.js
@@ -0,0 +1,25 @@
+const startBtnEl = document.querySelector('.btn')
+const homePageEl = document.querySelector('#home-page')
+const PjoQuizPageEl = document.querySelector('#pj-char-quiz-pg')
+const q1El = document.querySelector('#pj-q1')
+const q2El = document.querySelector('#pj-q2')
+
+// hide pjo quiz page
+PjoQuizPageEl.style.display = "none"
+
+// show pjo quiz page
+function startBtnHandler () {
+    homePageEl.style.display = "none"
+    PjoQuizPageEl.style.display = "block"
+    q2El.style.display = "none"
+}
+startBtnEl.addEventListener('click', startBtnHandler)
+
+// go to next question q1
+function nextQsHandler (event) {
+    if (event.target && event.target.matches('input[type=radio]')) {
+        q1El.style.display = "none"
+        q2El.style.display = "block"
+    }
+}
+q1El.addEventListener('click', nextQsHandler)
